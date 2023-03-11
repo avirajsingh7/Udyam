cbe61c3dad7ce6895e65962346fc71402c8a88fc
gupta-arpan <arpan6095@gmail.com>
2023-03-11 15:07:16 +0530

    UI Design updated.

--- app/src/main/java/com/example/udyam/landing_page.kt
+++ app/src/main/java/com/example/udyam/landing_page.kt
@@ -1,29 +1,11 @@
 package com.example.udyam
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.udyam.databinding.ActivityLandingPageBinding
 
 class landing_page : AppCompatActivity() {
-
-    private  lateinit var binding: ActivityLandingPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityLandingPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.button.setOnClickListener {
-            startActivity(Intent(this,login_activity::class.java))
-            finish()
-        }
-        binding.textView3.setOnClickListener {
-            startActivity(Intent(this,signup_activity::class.java))
-            finish()
-        }
-
-
-
+        setContentView(R.layout.activity_landing_page)
     }
 }
