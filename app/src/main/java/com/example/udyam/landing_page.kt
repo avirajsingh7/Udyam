0ade6fce3ea19d16dca0d7c2bb82e057ae2018ad
Aviraj Gour <asgour2021@gmail.com>
2023-03-14 16:08:13 +0530

    wroking fine

--- app/src/main/java/com/example/udyam/landing_page.kt
+++ app/src/main/java/com/example/udyam/landing_page.kt
@@ -11,20 +11,19 @@ class landing_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityLandingPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         binding.button.setOnClickListener {
             startActivity(Intent(this,login_activity::class.java))
             finish()
         }
-
         binding.textView3.setOnClickListener {
             startActivity(Intent(this,signup_activity::class.java))
             finish()
         }
+
+
 
     }
 }
