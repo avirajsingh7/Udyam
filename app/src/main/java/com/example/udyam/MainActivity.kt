dae9422a15c76345bb49e4f57b8c97403f77227c
Aviraj Gour <asgour2021@gmail.com>
2023-03-15 17:36:27 +0530

    Feature : Maps Integrated

--- app/src/main/java/com/example/udyam/MainActivity.kt
+++ app/src/main/java/com/example/udyam/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.setOnItemSelectedListener{
             when(it.itemId){
                 R.id.home ->replaceFragment(Home())
-                R.id.map ->replaceFragment(Map())
+                R.id.map ->replaceFragment(MapsFragment())
                 R.id.schedule ->replaceFragment(Schedule())
                 R.id.leaderboard ->replaceFragment(LeaderBoard())
                 R.id.profile ->replaceFragment(Profile())
