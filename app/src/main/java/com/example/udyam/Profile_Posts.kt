06b46292b5ecdb25e32ec72e6119847f917ebcba
Akanksha080 <21bma017@nith.ac.in>
2023-03-15 23:03:06 +0530

    Made the user profile fragment

--- app/src/main/java/com/example/udyam/Profile_Posts.kt
+++ app/src/main/java/com/example/udyam/Profile_Posts.kt
@@ -5,10 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,23 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
+ * Use the [Profile_Posts.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Profile : Fragment() {
+class Profile_Posts : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val viewPager = view.findViewById<ViewPager2>(R.id.profile_view_pager)
-        val adapter = ProfilePagerAdapter(childFragmentManager, lifecycle)
-        viewPager.adapter = adapter
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +34,8 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_profile__posts, container, false)
     }
-
-
-
 
     companion object {
         /**
@@ -61,12 +44,12 @@ class Profile : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
+         * @return A new instance of fragment Profile_Posts.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Profile().apply {
+            Profile_Posts().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -74,5 +57,3 @@ class Profile : Fragment() {
             }
     }
 }
-
-
